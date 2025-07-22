bbafcc501449ac934402b79ab698c7b7eb194a7e
SuperSon7 <vin990227@naver.com>
2025-07-22 23:14:21 +0900

    add docker and docker compose for CICD

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -92,11 +93,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'hart_db',         
-        'USER': 'admin',               
-        'PASSWORD': 'admin123',        
-        'HOST': 'localhost',           
-        'PORT': '5432',                
+        'NAME': config('DB_NAME'),         
+        'USER': config('DB_USER'),               
+        'PASSWORD': config('DB_PASSWORD'),        
+        'HOST': config('DB_HOST'),           
+        'PORT': config('DB_PORT'),                
     }
 }
 
