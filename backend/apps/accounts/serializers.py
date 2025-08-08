7ee03785c7d98423fac4a822249401575f740a60
SuperSon7 <vin990227@naver.com>
2025-08-08 22:39:41 +0900

    add utils module

--- backend/apps/accounts/serializers.py
+++ backend/apps/accounts/serializers.py
@@ -77,32 +77,6 @@ class LoginSerializer(serializers.Serializer):
             
             attrs['user'] = user
         return attrs
-class ProfileImageUploadSerializer(serializers.Serializer):
-    filename = serializers.CharField(max_length=255)
-    content_type = serializers.CharField(max_length=50)
-    
-    def validate(self, attrs):
-        filename = attrs.get('filename')
-        content_type = attrs.get('content_type')
-        
-        if not filename or not content_type:
-            raise serializers.ValidationError("Filename and content type are required")
-        
-        valid_content_types = {
-            'image/jpeg': ['.jpg', '.jpeg'],
-            'image/png': ['.png'],
-            'image/gif': ['.gif'],
-            'image/webp': ['.webp'],
-        }
-        if content_type not in valid_content_types:
-            raise serializers.ValidationError("Invalid content type")
-        
-        # check content type against file extension
-        file_ext = os.path.splitext(filename.lower())[1]
-        if file_ext not in valid_content_types[content_type]:
-            raise serializers.ValidationError("File extension doesn't match content type")
-        
-        return attrs
 
 class SocialLoginSerializer(serializers.Serializer):
     code = serializers.CharField(required=True)
