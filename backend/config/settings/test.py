5269c5ba48395e9ba2a74ed63fec712b512f6f15
SuperSon7 <vin990227@naver.com>
2025-08-14 22:07:38 +0900

    fix configurations for CI

--- backend/config/settings/test.py
+++ backend/config/settings/test.py
@@ -13,11 +13,10 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME", "test_db"),
         "USER": os.environ.get("DB_USER", "postgres"),
         "PASSWORD": os.environ.get("DB_PASSWORD", "postgres"),
         "HOST": os.environ.get("DB_HOST", "test_db"),
-        "PORT": os.environ.get("DB_PORT", 5433),
+        "PORT": os.environ.get("DB_PORT", 5432),
     }
 }
 
