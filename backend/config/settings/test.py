06f0c4cd395649d6faced8c444b38b858343c593
SuperSon7 <vin990227@naver.com>
2025-08-14 18:58:39 +0900

    change for git actions

--- backend/config/settings/test.py
+++ backend/config/settings/test.py
@@ -5,19 +5,19 @@ from dotenv import load_dotenv
 from .base import *  # noqa: E402, F403
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env.test"))
+load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env"))
 
 SECRET_KEY = os.getenv("SECRET_KEY", "dummy-test-secret-key")
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("DB_NAME", "test_db"),
-        'USER': os.environ.get("DB_USER", "postgres"),
-        'PASSWORD': os.environ.get("DB_PASSWORD", "postgres"),
-        'HOST': os.environ.get("DB_HOST", "localhost"),
-        'PORT': os.environ.get("DB_PORT", 5433),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME", "test_db"),
+        "USER": os.environ.get("DB_USER", "postgres"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "postgres"),
+        "HOST": os.environ.get("DB_HOST", "test_db"),
+        "PORT": os.environ.get("DB_PORT", 5433),
     }
 }
 
