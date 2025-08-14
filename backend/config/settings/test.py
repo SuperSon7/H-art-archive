bf79544f3aca909d04f2f54095914c6288589b15
SuperSon7 <vin990227@naver.com>
2025-08-14 22:30:59 +0900

    fuck...

--- backend/config/settings/test.py
+++ backend/config/settings/test.py
@@ -10,15 +10,6 @@ load_dotenv(dotenv_path=os.path.join(BASE_DIR, ".env"))
 SECRET_KEY = os.getenv("SECRET_KEY", "dummy-test-secret-key")
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "USER": os.environ.get("DB_USER", "postgres"),
-        "PASSWORD": os.environ.get("DB_PASSWORD", "postgres"),
-        "HOST": os.environ.get("DB_HOST", "test_db"),
-        "PORT": os.environ.get("DB_PORT", 5432),
-    }
-}
 
 EMAIL_BACKEND = os.getenv("EMAIL_BACKEND", "django.core.mail.backends.locmem.EmailBackend")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "noreply@test.com")
